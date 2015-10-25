ed2f3a5b49e64c6baae41e01a8cf91a728db6ce6
Rui Guo <rguo@leancloud.rocks>
2015-10-25 11:00:04 +0800

    (feature) RandomizedQueue

--- src/main/java/algo/assignment/queue/RandomizedQueue.java
+++ src/main/java/algo/assignment/queue/RandomizedQueue.java
@@ -1,6 +1,10 @@
 package algo.assignment.queue;
 
+import com.google.common.base.Preconditions;
+
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created on 15/10/14.
@@ -19,7 +23,7 @@ import java.util.Iterator;
  * }
  */
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    private static final int defaultInitCapacity = 16;
+    private static final int defaultMinCapacity = 2;
 
     private int size;
     private int capacity;
@@ -28,7 +32,7 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     private Item[] data;
 
     public RandomizedQueue(){
-        this(defaultInitCapacity);
+        this(defaultMinCapacity);
     }
 
     @SuppressWarnings("unchecked")
@@ -46,34 +50,118 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     }
 
     private int next(int index){
-        return ++index & (capacity - 1);
+        return nextN(index, 1);
+    }
+
+    private int nextN(int index, int n){
+        return (index + n) & (capacity - 1);
     }
 
     @SuppressWarnings("unchecked")
     private void resize(int newSize){
-        Item[] newData = (Item[])new Object[newSize];
-        int dst = 0;
-        int src = head;
-        while (next(src) == head){
-            newData[++dst] = data[src];
-            src = next(src);
+        if (newSize > defaultMinCapacity) {
+            Item[] newData = (Item[]) new Object[newSize];
+            int dst = 0;
+            int src = head;
+            while (src != tail) {
+                newData[dst++] = data[src];
+                src = next(src);
+            }
+
+            head = 0;
+            tail = dst;
+            data = newData;
+            capacity = newSize;
         }
-
-        head = 0;
-
     }
 
     public void enqueue(Item item){
+        Preconditions.checkNotNull(item);
+
         if (next(tail) == head){
             resize(capacity * 2);
         }
 
         data[tail] = item;
+        tail = next(tail);
+        ++size;
+    }
+
+    public Item dequeue(){
+        if (isEmpty()){
+            throw new NoSuchElementException();
+        }
+
+        int rand = ThreadLocalRandom.current().nextInt(size);
+        int randIndex = nextN(head, rand);
+        Item retItem = data[randIndex];
+
+        Item h = data[head];
+        if (randIndex != head) {
+            data[randIndex] = h;
+        }
+
+        data[head] = null;
+        head = next(head);
+
+        --size;
+
+        if (size == capacity / 4){
+            resize(capacity / 4);
+        }
+
+        return retItem;
     }
 
     @Override
     public Iterator<Item> iterator() {
-        return null;
+        return new Itr(head);
+    }
+
+    private class Itr implements Iterator<Item>{
+        private int cursor;
+
+        public Itr(int head){
+            this.cursor = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return cursor != tail;
+        }
+
+        @Override
+        public Item next() {
+            Item item = data[cursor];
+            cursor = RandomizedQueue.this.next(cursor);
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        RandomizedQueue<Integer> q = new RandomizedQueue<>();
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+
+        for (Integer i : q){
+            System.out.println(i);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(q.dequeue());
+        }
+
+        for (Integer i : q){
+            System.out.println(i);
+        }
+
+        try {
+            q.dequeue();
+        } catch (NoSuchElementException e){
+            System.out.println("catch no element exception");
+        }
     }
 }
 
